eef81fe5e2358784b0773f882401441a14424be1
Angel-dash <njlghmr@gmail.com>
2023-08-28 20:33:33 +0545

    functionality to add experiences

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ const page = () => {
 			<h1>This is the home page</h1>
 			<Info />
 			<Experience />
+			<Experience />
 		</div>
 	);
 };
--- components/experience.js
+++ components/experience.js
@@ -3,14 +3,39 @@ import React from "react";
 import { useState } from "react";
 export const Experience = () => {
 	const [experience, setExperience] = useState("");
+	const [duration, setDuration] = useState("");
+	const [position, setPosition] = useState("");
+	const [company, setCompany] = useState("");
 	return (
 		<div>
 			<label>Experience:</label>
+			<br />
 			<input
 				type='text'
-				placeholder='Experience  '
+				placeholder='Position: '
+				value={position}
+				onChange={(event) => setPosition(event.target.value)}
+			/>
+			<br />
+			<input
+				type='text'
+				placeholder='Duration: '
+				value={duration}
+				onChange={(event) => setDuration(event.target.value)}
+			/>
+			<br />
+			<input
+				type='text'
+				placeholder='Job Description  '
 				value={experience}
 				onChange={(event) => setExperience(event.target.value)}
+			/>
+			<br />
+			<input
+				type='text'
+				placeholder='Company Name: '
+				value={company}
+				onChange={(event) => setCompany(event.target.value)}
 			/>
 		</div>
 	);
