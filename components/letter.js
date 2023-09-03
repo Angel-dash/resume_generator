e20c517daa5007dca268cc7ebd41affd86e9091c
Angel-dash <njlghmr@gmail.com>
2023-09-03 15:46:31 +0545

    CV letter styling

--- components/letter.js
+++ components/letter.js
@@ -4,29 +4,37 @@ export const InfoCard = (props) => {
 	const { education, experience, info } = props;
 
 	return (
-		<div className='bg-slate-700 py-10 px-10 ml-20 mr-20 mt-20 mb-20 min-w-fit min-h-screen sm:px-5 md:px-20 lg:px-40 '>
-			<title className='text-5xl text-black'>
+		<div className='bg-gray-100 p-10 mx-auto max-w-4xl sm:p-5 md:p-20 lg:p-40'>
+			<h1 className='text-5xl text-gray-800 font-bold'>
 				I'M {info.firstName} {info.lastName}
-			</title>
-			<div className='bg-red-50'>
-				<h1 className=' text-6xl  text-teal-600 px-4 font-sans'>Profile</h1>
-				<div className='flex justify-between'>
+			</h1>
+			<div className='bg-white shadow-lg rounded-lg mt-10 p-10'>
+				<h2 className='text-4xl text-teal-600 font-semibold'>Profile</h2>
+				<div className='grid grid-cols-2 gap-4 mt-4'>
 					<p>
-						Name:{info.firstName} {info.lastName}
+						<span className='font-bold'>Name:</span> {info.firstName}{" "}
+						{info.lastName}
 					</p>
-					<p>Email :{info.EmaiId}</p>
-					<p>Contact:{info.phoneNumber}</p>
-					<p>Address :{info.Address}</p>
-					<p>Date of birth :{info.DOB}</p>
-					<p>LinkedIn :{info.linkedin}</p>
+					<p>
+						<span className='font-bold'>Email:</span> {info.EmaiId}
+					</p>
+					<p>
+						<span className='font-bold'>Contact:</span> {info.phoneNumber}
+					</p>
+					<p>
+						<span className='font-bold'>Address:</span> {info.Address}
+					</p>
+					<p>
+						<span className='font-bold'>Date of birth:</span> {info.DOB}
+					</p>
+					<p>
+						<span className='font-bold'>LinkedIn:</span> {info.linkedin}
+					</p>
 				</div>
 			</div>
-			<br />
-			<div className='bg-red-50'>
-				<h2 className='px-4 text-6xl text-red-700 font-medium  font-sans'>
-					Education
-				</h2>
-				<div className=' py-4 px-4'>
+			<div className='bg-white shadow-lg rounded-lg mt-10 p-10'>
+				<h3 className='text-4xl text-red-700 font-semibold'>Education</h3>
+				<div className='mt-4'>
 					<p>
 						I am proud to have graduated from {education.uniName} with a degree
 						in {education.degree}. My educational journey began in{" "}
@@ -52,25 +60,25 @@ export const InfoCard = (props) => {
 				</div>
 			</div>
 
-			<div>
-				<h3 className='text-3xl py-2 px-4 text-red-700 font-medium md:text-6xl'>
-					Experience
-				</h3>
-				<p>
-					In my previous role as a {experience.Position} at {experience.Company}
-					, I had the privilege of contributing to a dynamic team and enhancing
-					my skill set. Throughout my {experience.Duration} with the company, I
-					gained valuable experience and worked on a range of projects that
-					allowed me to deepen my understanding of {experience.Experience}. It
-					was an incredible opportunity to collaborate with talented
-					professionals and to immerse myself in projects that challenged me to
-					innovate and grow. I am proud to have been a part of{" "}
-					{experience.Company} and to have played a role in creating impactful
-					digital solutions. This experience has further fueled my passion for
-					frontend development and inspired me to continue pushing the
-					boundaries of what's possible in the ever-evolving world of
-					technology.
-				</p>
+			<div className='bg-white shadow-lg rounded-lg mt-10 p-10'>
+				<h4 className='text-4xl text-red-700 font-semibold'>Experience</h4>
+				<div className='mt-4'>
+					<p>
+						In my previous role as a {experience.Position} at{" "}
+						{experience.Company}, I had the privilege of contributing to a
+						dynamic team and enhancing my skill set. Throughout my{" "}
+						{experience.Duration} with the company, I gained valuable experience
+						and worked on a range of projects that allowed me to deepen my
+						understanding of {experience.Experience}. It was an incredible
+						opportunity to collaborate with talented professionals and to
+						immerse myself in projects that challenged me to innovate and grow.
+						I am proud to have been a part of {experience.Company} and to have
+						played a role in creating impactful digital solutions. This
+						experience has further fueled my passion for frontend development
+						and inspired me to continue pushing the boundaries of what's
+						possible in the ever-evolving world of technology.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
